a143a3ae8ebc6d886e8d98c2bf6a782f96decf4f
TreetheFirst <126304980+TreetheFirst@users.noreply.github.com>
2023-10-11 20:21:41 -0500

    Delete class_db.py

--- class_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import openpyxl
-
-dataframe1 = openpyxl.load_workbook('/Users/Archith/Downloads/Class_list.xlsx')
-dataframe1 = dataframe1.active
-m_row = dataframe1.max_row
-
-print(dataframe1.columns)
-
